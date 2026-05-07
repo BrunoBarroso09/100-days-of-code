d5030c26967da9f34a3e8060008f6527d9ca3f70
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-05-07 21:14:01 +0100

    Build the Snake Game Part 1: Animation & Coordinates

--- Day_20/main.py
+++ Day_20/main.py
@@ -8,13 +8,19 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake()#Create snake
-
+#Create snake
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    snake.move()# Move snake
+    # Move snake
+    snake.move()
 
 screen.exitonclick()
--- README.md
+++ README.md
@@ -22,3 +22,4 @@
 - [Day 17:](https://github.com/BrunoBarroso09/100-days-of-code/tree/Dev/Day_17/main.py) Quiz project</h4>
 - [Day 18:](https://github.com/BrunoBarroso09/100-days-of-code/tree/Dev/Day_18/main.py) Painting Project</h4>
 - [Day 19:](https://github.com/BrunoBarroso09/100-days-of-code/tree/Dev/Day_19/main.py) Turtle race</h4>
+- [Day 20:](https://github.com/BrunoBarroso09/100-days-of-code/tree/Dev/Day_20/main.py) Snake game</h4>
