a7e5f7adeb5e7168e9b5244da7b46d0c4f3f9d1d
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-07-05 20:46:02 +0100

    List comprehension and Nato Alphabet

---
+++ Day_26/NatoAlphabet/main.py
@@ -0,0 +1,8 @@
+import pandas
+
+data = pandas.read_csv("nato_alphabet.csv")
+
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+word = input("Enter a word: ").upper()
+answer = [data_dict[letter] for letter in word]
+print(answer)
